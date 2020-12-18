90f8743cf400e58f504c09944664bc1e59f11500
Kimberly <kmglock@mail.lipscomb.edu>
2020-12-18 16:22:25 -0600

    Done.

--- lib/values.rb
+++ lib/values.rb
@@ -1,5 +1,8 @@
 require 'pry'
 
+
+# https://ruby-doc.org/core-2.7.2/Hash.html
+
 #This is the groceries hash we'll be passing in to the get_the_values method
 # groceries = {
 #  dairy: ["milk", "yogurt", "cheese"],
@@ -9,5 +12,6 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  #code your solution here!
+  #groceries.values # returns an array of values for each key, so multiple arrays
+  groceries.values.flatten # returns one array with all values inside due to .flatten
 end
